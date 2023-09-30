cff3815e33127b6763ac5de1f515ac99b5731187
Fern <58086702+F3rny@users.noreply.github.com>
2023-09-30 09:03:51 -0600

    Update Lines.py

--- Lines.py
+++ Lines.py
@@ -9,12 +9,18 @@ color = (200,155,64)
 pos1 = (20,20)
 pos2 = (150,143)
 
+points = [] # empty list 
+
 while True:
   for event in pygame.event.get():
     if event.type == QUIT:
       pygame.quit()
       sys.exit()
 
+    if event.type == MOUSEBUTTONDOWN:
+      points.append(event.pos)
+  if len(points) > 1:
+    pygame.draw.lines(screen, color, False, points, 3) ## line drawing 
   screen.lock()
   pygame.draw.line(screen, color, pos1, pos2, 3) ## create line
   screen.unlock()
